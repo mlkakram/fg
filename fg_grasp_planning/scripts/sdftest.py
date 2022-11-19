f50bfeb3d71ba44852b4ebb6f162638cd4b0b858
Malek <mk.aburrub@gmail.com>
2022-11-19 19:59:25 +0900

    testing the skfmm library

--- fg_grasp_planning/scripts/sdftest.py
+++ fg_grasp_planning/scripts/sdftest.py
@@ -3,9 +3,8 @@
 import skfmm
 import numpy as np 
 
-phi = np.ones((3,3))
-phi[1,1] = -1
-
-x = skfmm.distance(phi)
-
-print(x)
+X, Y = np.meshgrid(np.linspace(-1,1,200), np.linspace(-1,1,200))
+phi = -1 * np.ones_like(X)
+phi[X > -0.5] = 1
+phi[np.logical_and(np.abs(Y) < 0.25, X > -0.75)] = 1
+d = skfmm.distance(phi, dx=1e-2)
